9247368a0e8da7230ee25ba52b95a0bc5b178093
wingsiu <wingsiu48@gmail.com>
2025-10-02 06:46:38 +0800

    Add Fonts

--- Package.swift
+++ Package.swift
@@ -15,7 +15,12 @@ let package = Package(
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "AlphaEqt"),
+            name: "AlphaEqt",
+            resources: [
+                    .process("Fonts"),
+                ]
+        ),
+        
         .testTarget(
             name: "AlphaEqtTests",
             dependencies: ["AlphaEqt"]
